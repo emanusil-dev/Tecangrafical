31052334fdab3792f882cc53acfd1361944c0608
Emanuel da Silva <emanusilva279@gmail.com>
2025-06-17 11:21:42 +0100

    Delete assets directory

--- assets/js/forms.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Message sent! (Here you can handle the form submission logic)');
-  });
-  
