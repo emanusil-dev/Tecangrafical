1d6a3548d4b555c292ce955d53c08fbad0e5dd16
Emanuel da Silva <emanusilva279@gmail.com>
2025-07-03 09:17:56 +0100

    Add files via upload

---
+++ assets/js/forms.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Message sent! (Here you can handle the form submission logic)');
+  });
+  
